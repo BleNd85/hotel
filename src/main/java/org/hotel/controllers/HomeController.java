8a159234e086f36ceedbcb9f7e80a2db269a16b5
Sokolov Evgeniy <159652993+Ferox6@users.noreply.github.com>
2024-05-24 18:27:14 +0300

    +lox

--- src/main/java/org/hotel/controllers/HomeController.java
+++ src/main/java/org/hotel/controllers/HomeController.java
@@ -1,15 +1,37 @@
 package org.hotel.controllers;
 
+import ch.qos.logback.core.model.Model;
+import org.hotel.models.HotelModel;
+import org.hotel.services.HotelService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @RequestMapping("/")
 @Controller
 public class HomeController {
+    private final HotelService hotelService;
+
+    public HomeController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
     @GetMapping
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        List<HotelModel> hotelModelList = hotelService.getAll();
+        int amountOfPlaces = 0;
+        for (HotelModel hotelModel : hotelModelList) {
+            amountOfPlaces += hotelModel.getAmountOfPlaces();
+
+
+        }
+
+
+
         return "home_page";
+
+
     }
 
 }
